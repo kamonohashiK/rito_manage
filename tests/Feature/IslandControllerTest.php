4f9cdd941ce0acefee1b9aedac87eb5542bcbb84
kamonohashiK <kamonohashiy@gmail.com>
2023-10-31 20:03:37 +0900

    #24 コントローラとテストコードの叩き台を作成

---
+++ tests/Feature/IslandControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Tests\TestCase;
+
+class IslandControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * ログインしている場合は、島の一覧画面が表示されるかテスト
+     *
+     * @return void
+     */
+    public function test_index_page(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * ログインしていない場合は、ログイン画面にリダイレクトされるかテスト
+     *
+     * @return void
+     */
+    public function test_index_page_when_not_login(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+}
