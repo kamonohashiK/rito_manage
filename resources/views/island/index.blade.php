3588b6d49b240fa279c734c39d0f4d40defc0975
kamonohashiK <kamonohashiy@gmail.com>
2023-11-01 13:33:55 +0900

    #26 テーブル部分をコンポーネントに分離

--- resources/views/island/index.blade.php
+++ resources/views/island/index.blade.php
@@ -7,47 +7,7 @@
 @stop
 
 @section('content')
-    <?php //TODO: 丸ごとコンポーネントに切り出す
-    ?>
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">
-                        <div class="input-group input-group-sm" style="width: 150px;">
-                            <input type="text" name="table_search" class="form-control" placeholder="Search">
-                            <div class="input-group-append">
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </h3>
-                </div>
-                <div class="card-body table-responsive p-0">
-                    <table class="table table-hover text-nowrap">
-                        <thead>
-                            <tr>
-                                <th>島名</th>
-                                <th>都道府県名</th>
-                                <th>市区町村名</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($islands as $island): ?>
-                            <tr>
-                                <td><a href="{{ route('islands.show', ['id' => $island->id]) }}">{{ $island->name }}</a>
-                                </td>
-                                <td>{{ $island->prefecture_name }}</td>
-                                <td>{{ $island->city_name }}</td>
-                            </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-islands-table :islands="$islands" />
 
     @if ($islands->hasPages())
         <div class="card-footer clearfix">
