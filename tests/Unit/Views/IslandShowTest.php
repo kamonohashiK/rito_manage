57f6b965044b6ef2c6f92adae57d9413d6f1a7e7
kamonohashiK <kamonohashiy@gmail.com>
2023-11-09 07:47:46 +0900

    #62 画面上に表示

--- tests/Unit/Views/IslandShowTest.php
+++ tests/Unit/Views/IslandShowTest.php
@@ -2,15 +2,17 @@
 
 namespace Tests\Feature\Views;
 
+use App\Models\Answer;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Island;
 use App\Models\Prefecture;
 use App\Models\City;
+use App\Models\Question;
 use Illuminate\Support\Facades\DB;
 
-class IslandShow extends TestCase
+class IslandShowTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -39,7 +41,7 @@ class IslandShow extends TestCase
      */
     public function test_show_page(): void
     {
-        $response = $this->actingAs($this->user)->get("/island/{$this->island->id}");
+        $response = $this->actingAs($this->user)->get("/islands/{$this->island->id}");
 
         $response->assertStatus(200);
         $response->assertViewHas('island')
@@ -60,7 +62,8 @@ class IslandShow extends TestCase
             ->assertSee("<th>緯度</th>", false)
             ->assertSee($this->island->lat, false)
             ->assertSee("<th>経度</th>", false)
-            ->assertSee($this->island->lng, false);
+            ->assertSee($this->island->lng, false)
+            ->assertSee("質問", false);
     }
 
     /**
@@ -74,8 +77,59 @@ class IslandShow extends TestCase
             'island_id' => $this->island->id,
         ]);
 
-        $response = $this->actingAs($this->user)->get("/island/{$this->island->id}");
+        $response = $this->actingAs($this->user)->get("/islands/{$this->island->id}");
         $response->assertSee("<th>市区町村</th>", false)
             ->assertSee("{$this->city->name} 他", false);
+    }
+
+    /**
+     * 質問がある場合は、質問が表示されるかテスト
+     */
+    public function test_show_page_with_questions()
+    {
+        $questions = Question::factory(3)->for($this->island)->create();
+        $response = $this->actingAs($this->user)->get("/islands/{$this->island->id}");
+        $response->assertSee("質問", false)
+            ->assertSee($questions[0]->question, false)
+            ->assertSee($questions[1]->question, false)
+            ->assertSee($questions[2]->question, false);
+    }
+
+    /**
+     * 質問がない場合は、その旨の文言が表示されるかテスト
+     */
+    public function test_show_page_without_questions()
+    {
+        $response = $this->actingAs($this->user)->get("/islands/{$this->island->id}");
+        $response->assertSee("質問", false)
+            ->assertSee("この島に関する質問はありません。", false);
+    }
+
+    /**
+     * 質問に回答がある場合は、質問の回答が表示されるかテスト
+     */
+    public function test_show_page_with_answers()
+    {
+        $question = Question::factory()->for($this->island)->create();
+        $answers = Answer::factory(3)->for($question)->create();
+
+        $response = $this->actingAs($this->user)->get("/islands/{$this->island->id}");
+        $response->assertSee("質問", false)
+            ->assertSee($answers[0]->answer, false)
+            ->assertSee($answers[1]->answer, false)
+            ->assertSee($answers[2]->answer, false);
+    }
+
+    /**
+     * 質問に回答がない場合は、その旨の文言が表示されるかテスト
+     */
+    public function test_show_page_without_answers()
+    {
+        $question = Question::factory()->for($this->island)->create();
+
+        $response = $this->actingAs($this->user)->get("/islands/{$this->island->id}");
+        $response->assertSee("質問", false)
+            ->assertSee($question->question, false)
+            ->assertSee("この質問に対する回答はありません。", false);
     }
 }
